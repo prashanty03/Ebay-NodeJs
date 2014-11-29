1e19d9e6da4f0543580658c3c996cfe648add5ba
Meher <MeherFarozan@users.noreply.github.com>
2014-11-29 14:11:39 -0800

    Update meher.js
    Added support for Active/ Inactive Status, check quantity >0 and calculates bid end time while getProducts

--- routes/meher.js
+++ routes/meher.js
@@ -35,7 +35,8 @@ exports.updateProduct = function(req, res) {
 					if (err)
 						console.log("Error getting vlaues % s", err);
 					connection.end();
-					console.log(rows);
+					//console.log(rows);
+					console.log(rows[0].name.length);
 					res.render('updateProduct', {
 						page_title : "Update Product",
 						data : rows,
@@ -59,9 +60,55 @@ exports.saveUpdatedProduct = function(req, res) {
 				+ ' in saveUpdated******************');
 		var condition = input.condition == "1000" ? "New" : "Refurbished";
 		var auction = input.format == "Auction" ? 1 : 0;
-		var temp_path = req.files.image.path;
-		console.log(temp_path);
-		var fs = require('fs');
+		var active = input.format == "Active" ? 1 : 0;
+		var myDate = new Date();
+		myDate.setDate(myDate.getDate() + parseInt(input.duration));
+		//var temp_path = req.files.image.path;
+		//console.log(temp_path);
+		//var fs = require('fs');
+		//		var data = {
+		//			name : input.title,
+		//			details : input.details,
+		//			condition : condition,
+		//			isForAuction : auction,
+		//			min_bid : input.startPrice * 1,
+		//			quantity : parseInt(input.quantity),
+		//			bid_duration : parseInt(input.duration),
+		//			category_id : input.categoryId,
+		//			cost : input.startPrice * 1,
+		//			bid_start_time : new Date(),
+		//			image : input.image
+		//		};
+		//		// console.log(data);
+
+		//var msg = validate(input, req.files.image.name);
+		//console.log(msg);
+		//console.log("Message : " + msg.length);
+		//if (msg.length == 0) {
+
+		console.log("inside if")
+		// get the temporary location of the file
+		//var tmp_path = req.files.image.path;
+		// set where the file should actually exists - in this case it is in
+		// the
+		// "images" directory
+		//	var target_path = './public/images/' + req.files.image.name;
+		// move the file from the temporary location to the intended
+		// location
+		//fs.rename(tmp_path, target_path, function(err) {
+		//if (err)
+		//throw err;
+		// delete the temporary file, so that the explicitly set
+		// temporary
+		// upload dir does not get filled with unwanted files
+		//	fs.unlink(tmp_path, function() {
+		//	if (err)
+		//	throw err;
+		//	console.log();
+		// res.send('File uploaded to: ' + target_path + ' - ' +
+		// req.files.image.size + ' bytes');
+		//});
+		//});
 		var data = {
 			name : input.title,
 			details : input.details,
@@ -70,88 +117,50 @@ exports.saveUpdatedProduct = function(req, res) {
 			min_bid : input.startPrice * 1,
 			quantity : parseInt(input.quantity),
 			bid_duration : parseInt(input.duration),
-			category_id : input.categoryId,
+			//category_id : input.categoryId,
 			cost : input.startPrice * 1,
 			bid_start_time : new Date(),
-			image : input.image
+			isActive : active,
+			bid_end_time : myDate
+		//image : target_path.substring(8)
 		};
-		// console.log(data);
+		connection.connect();
+		var query = connection.query("update products set ? where id = ?", [
+				data, productId ], function(err, info) {
+			if (err) {
+				console.log("Error inserting : %s", err);
+				req.flash('error', msg);
+				res.redirect('/updateProduct');
+			} else {
+				res.redirect('/getSellerProducts');
+			}
 
-		var msg = validate(input, req.files.image.name);
-		console.log(msg);
-		console.log("Message : " + msg.length);
-		if (msg.length == 0) {
-
-			console.log("inside if")
-			// get the temporary location of the file
-			var tmp_path = req.files.image.path;
-			// set where the file should actually exists - in this case it is in
-			// the
-			// "images" directory
-			var target_path = './public/images/' + req.files.image.name;
-			// move the file from the temporary location to the intended
-			// location
-			fs.rename(tmp_path, target_path, function(err) {
-				if (err)
-					throw err;
-				// delete the temporary file, so that the explicitly set
-				// temporary
-				// upload dir does not get filled with unwanted files
-				fs.unlink(tmp_path, function() {
-					if (err)
-						throw err;
-					console.log();
-					// res.send('File uploaded to: ' + target_path + ' - ' +
-					// req.files.image.size + ' bytes');
-				});
-			});
-			var data = {
-				name : input.title,
-				details : input.details,
-				condition : condition,
-				isForAuction : auction,
-				min_bid : input.startPrice * 1,
-				quantity : parseInt(input.quantity),
-				bid_duration : parseInt(input.duration),
-				category_id : input.categoryId,
-				cost : input.startPrice * 1,
-				bid_start_time : new Date(),
-				image : target_path.substring(8)
-			};
-			connection.connect();
-			var query = connection.query("update products set ? where id = ?",
-					[ data, productId ], function(err, info) {
-						if (err)
-							console.log("Error inserting : %s", err);
-						else {
-							res.redirect('/getSellerProducts');
-						}
-
-					});
-			console.log(query);
-			connection.end();
-		} else {
-			console.log("inside else")
-			req.flash('error', msg);
-			res.redirect('/updateProduct');
-		}
-
-		// set where the file should actually exists - in this case it is in the
-		// "images" directory
-		// var target_path = '/images/' + req.files.image.name;
-		// move the file from the temporary location to the intended location
-		// fs.rename(tmp_path, target_path, function(err) {
-		// if (err) throw err;
-		// // delete the temporary file, so that the explicitly set temporary
-		// upload
-		// dir does not get filled with unwanted files
-		// fs.unlink(tmp_path, function() {
-		// if (err) throw err;
-		// res.send('File uploaded to: ' + target_path + ' - ' +
-		// req.files.thumbnail.size + ' bytes');
-		// });
-		// });
+		});
+		console.log(query);
+		connection.end();
 	}
+	//	else {
+	//			console.log("inside else")
+	//			req.flash('error', msg);
+	//			res.redirect('/updateProduct');
+	//		}
+
+	// set where the file should actually exists - in this case it is in the
+	// "images" directory
+	// var target_path = '/images/' + req.files.image.name;
+	// move the file from the temporary location to the intended location
+	// fs.rename(tmp_path, target_path, function(err) {
+	// if (err) throw err;
+	// // delete the temporary file, so that the explicitly set temporary
+	// upload
+	// dir does not get filled with unwanted files
+	// fs.unlink(tmp_path, function() {
+	// if (err) throw err;
+	// res.send('File uploaded to: ' + target_path + ' - ' +
+	// req.files.thumbnail.size + ' bytes');
+	// });
+	// });
+
 }
 
 exports.getProducts = function(req, res) {
@@ -166,7 +175,7 @@ exports.getProducts = function(req, res) {
 
 		var query = connection
 				.query(
-						"Select * from products where category_id = (select id from category where name = ?)",
+						"Select * from products where isActive='1' and quantity>'0' and category_id = (select id from category where name = ?)",
 						[ category_name ],
 						function(err, rows) {
 							if (err)
@@ -191,55 +200,18 @@ exports.getSellerProducts = function(req, res) {
 		var connection = mysqldb.getConnection();
 		connection.connect();
 
-		var query = connection.query(
-				"Select p.*, c.name as cname from products p join category c on c.id = p.category_id where p.seller_id = ?", seller_id,
-				function(err, rows) {
-					if (err)
-						console.log("Error fetching results : %s", err);
-					connection.end();
-					res.render('getSellerProducts', {
-						page_title : "Listed Products",
-						data : rows
-					});
-				});
+		var query = connection
+				.query(
+						"Select p.*, c.name as cname from products p join category c on c.id = p.category_id where p.seller_id = ?",
+						seller_id,
+						function(err, rows) {
+							if (err)
+								console.log("Error fetching results : %s", err);
+							connection.end();
+							res.render('getSellerProducts', {
+								page_title : "Listed Products",
+								data : rows
+							});
+						});
 	}
 };
-
-validate = function(input, name) {
-
-	var msgappender = " is mandoatory";
-	var msg = "";
-	console.log(typeof (input.startPrice * 1));
-	console.log(name.toString());
-	console.log(name.toString().indexOf("JPEG") != -1);
-	console.log(name.toString().indexOf("jpeg") != -1);
-	console.log(name.toString().indexOf("JPG") != -1);
-	console.log(name.toString().indexOf("jpg") != -1);
-	console.log(name.toString().indexOf("PNG") != -1);
-	console.log(name.toString().indexOf("png") != -1);
-
-	if (input.title == "" || input.title.length == 0) {
-		msg = "Title" + msgappender;
-	} else if (input.details == "" || input.details.length == 0) {
-		msg = "Description" + msgappender;
-	} else if (input.condition == "" || input.condition.length == 0)
-		msg = "Condition" + msgappender;
-	else if (input.format == "" || input.format.length == 0)
-		msg = "List As" + msgappender;
-	else if (input.startPrice == "" || input.condition.length == 0)
-		msg = "Start Price" + msgappender;
-	else if (input.duration == "" || input.duration.length == 0)
-		msg = "Duration" + msgappender;
-	else if (name.toString().indexOf(".") == -1)
-		msg = "Invalid Image";
-	// else if(!(name.toString().indexOf("JPEG")!=-1 ||
-	// name.toString().indexOf("jpeg")!=-1
-	// || name.toString().indexOf("JPG")!=-1 ||
-	// name.toString().indexOf("jpg")!=-1
-	// || name.toString().indexOf("png")!=-1)){
-	// msg = "Only JPEG,JPG and PNG Image supported";
-	// }
-	else if ((input.startPrice * 1) == NaN)
-		msg == "Invalid Start Price";
-	return msg;
-}
